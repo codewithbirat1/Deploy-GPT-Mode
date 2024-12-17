4fdb50d585906bc6fca07963d4212176d963de9d
codewithbirat1 <nexolinxs@gmail.com>
2024-12-17 15:50:22 +0545

    gyanAI

--- client/script.js
+++ client/script.js
@@ -75,7 +75,7 @@ async function handleSubmit(e) {
     loader(messageDiv);
 
     try {
-        const response = await fetch('http://localhost:5000/generate', {
+        const response = await fetch('https://sajilo-ai.onrender.com/generate', {
             method: 'POST',
             headers: { 'Content-Type': 'application/json' },
             body: JSON.stringify({ prompt: userMessage })
