af4d364f4f522b855c6374be10d13580ff239faf
codewithbirat1 <nexolinxs@gmail.com>
2024-12-17 16:12:01 +0545

    last

--- client/script.js
+++ client/script.js
@@ -75,7 +75,7 @@ async function handleSubmit(e) {
     loader(messageDiv);
 
     try {
-        const response = await fetch(' https://sajilo-ai.onrender.com/generate', {
+        const response = await fetch('https://sajilo-ai.onrender.com', {
             method: 'POST',
             headers: { 'Content-Type': 'application/json' },
             body: JSON.stringify({ prompt: userMessage }),
